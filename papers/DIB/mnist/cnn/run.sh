e34945403fcc5e0a97f24d74a569a230d3e1afd5
Alan Mosca <nitbix@nitbix.com>
2017-04-04 13:07:58 +0100

    add DIB

---
+++ papers/DIB/mnist/cnn/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+results_table="armsprop"
+results_db="amosca02"
+results_host="gpuvm1"
+
+if [ "$2" != "" ]; then
+  random_seeds=`tail -n +$2 random_seeds`
+else
+  random_seeds=`cat random_seeds`
+fi
+  echo $random_seed
+for exp in $@; do
+  echo $exp
+  for random_seed in $random_seeds; do
+    ~/git/toupee-keras/examples/mlp.py $exp --seed=$random_seed --results-table=$results_table --results-db=$results_db --results-host=$results_host
+  done
+done
