5da2015c44b45ae34abfa2fbb34b76d915b78cfa
Alan Mosca <nitbix@nitbix.com>
2018-07-24 14:34:01 +0100

    add tinyimagenet

---
+++ papers/NCA/tinyimagenet/cnn/output/make_single_graph.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+first_file=$1
+out_file=$2
+error_name=$3
+title=$4
+first_label=$5
+epochs=$6
+first_arg=2
+
+dumped_dir=/var/www/html/dumped/graphs
+
+echo "
+#set style line 1 lc rgb '#8b1a0e' lw 2 # --- red
+#set style line 2 lc rgb '#5e9c36' lw 2 # --- green
+set style line 1 lc rgb '#339933' lw 2 # --- green
+set style line 2 lc rgb '#0033AA' lw 2 # --- blue
+set style line 11 lc rgb '#808080' lt 1
+set border 3 back ls 11
+set tics nomirror
+set style line 12 lc rgb '#808080' lt 0 lw 1
+set grid back ls 12
+set title \"$title\"
+set terminal postscript eps enhanced color size 4,2.5
+set xlabel \"Boosting round\"
+set ylabel \"${error_name} (%)\"
+set terminal postscript eps enhanced color size 4,2.5
+set out \"${out_file}.eps\"
+plot \"${first_file}\"  every ::0::${epochs} using 1:$first_arg linestyle 1 title \"${first_label}\" with lines
+
+set terminal png
+set out \"${out_file}.png\"
+replot
+" | gnuplot
+mkdir -p ${dumped_dir}
+cp ${out_file}.png ${dumped_dir}
